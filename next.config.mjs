2c87024675a16717c8bef4e560c1808f663af489
Shivam Gupta <ishivamg@outlook.com>
2026-05-27 22:32:35 +0530

    Configure basePath and assetPrefix in next.config
    Set base path and asset prefix for deployment.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  basePath: '/shreyanshi-portfolio',
+  assetPrefix: '/shreyanshi-portfolio/',
+
   reactStrictMode: true,
 
   images: {
